cd1d1bd7c687230db2d9b16584dbecea09b26ca1
Nik_Dev <cherevan.n.s@gmail.com>
2019-08-12 02:26:23 +0300

    create button

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 document.getElementById('buttonSend').onclick = function (){
   var parentElement = document.getElementById('ul-code');
-var elemdiv = parentElement.insertAdjacentHTML('beforeend',"<li>"+document.getElementById('otziv').value+"</li>");
-
+  parentElement.insertAdjacentHTML('beforeend',"<li>"+document.getElementById('otziv').value+"</li>");
+  
 }
