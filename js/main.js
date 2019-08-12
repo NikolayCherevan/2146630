b290b29de9f0246dbb6135ee5008037b5a2d931d
Nik_Dev <cherevan.n.s@gmail.com>
2019-08-12 15:32:46 +0300

    added form

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 document.getElementById('buttonSend').onclick = function (){
   var parentElement = document.getElementById('ul-code');
-  parentElement.insertAdjacentHTML('beforeend',"<li>"+document.getElementById('otziv').value+"</li>");
-  
+  parentElement.insertAdjacentHTML('beforeend',"<p>"+document.getElementById('otziv').value+"</p>");
+  parentElement.insertAdjacentHTML('beforeend',"<Li>"+document.getElementById('imya').value+"</li>");
 }
